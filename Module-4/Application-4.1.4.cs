dae286d47865ed3c36a109da63e9a1012837c57b
Tamer <tamer.masud@mail.ru>
2024-02-12 13:50:25 +0400

    Добавьте файлы проекта.

---
+++ Module-4/Application-4.1.4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Module_4
+{
+    class Application_4_1_4
+    {
+        static void Main(string[] args)
+        {
+            string A, B;
+            bool C;
+
+            C = A != B;
+        }
+    }
+}
---
+++ Module-4/Application-4.1.5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module_4
+{
+    class Application_4_1_5
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            double X, Y;
+            bool C;
+
+            C = (A < B) || (X > Y); 
+        }
+    }
+}
