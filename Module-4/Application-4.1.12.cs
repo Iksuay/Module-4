4604fa66320bb4ab62a7240bac8877cd43696d99
Tamer <tamer.masud@mail.ru>
2024-02-12 16:41:14 +0400

    Добавлно задание 4.1.18

---
+++ Module-4/Application-4.1.12.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module_4
+{
+    class Application_4_1_12
+    {
+        static void Main(string[] args)
+        {
+
+            var a = 6;
+            var b = 7;
+
+            if (a != b)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+
+        }
+    }
+}
