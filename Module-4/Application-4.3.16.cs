10740542d24e2c20fafd608059e786c63bab66f1
Tamer <tamer.masud@mail.ru>
2024-02-15 23:28:56 +0400

    Добавлено задание 4.3.16

--- Module-4/Application-4.3.16.cs
+++ Module-4/Application-4.3.16.cs
@@ -3,17 +3,17 @@ using System;
 
 namespace Module_4
 {
-    internal class Application_4_3_15
+    internal class Application_4_3_16
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { -5, 6, 9, 1, 2, -3 };
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
             int num = 0;
             foreach (int i in arr)
             {
                 if (i > 0)
                 {
-                    a++;
+                    num++;
                 }
             }
 
