b1fa8abd605bc48284a1bd3b2446bc71bd1becc7
Tamer <tamer.masud@mail.ru>
2024-02-15 23:12:06 +0400

    Добавлено задание 4.3.15

---
+++ Module-4/Application-4.3.15.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module_4
+{
+    internal class Application_4_3_15
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int []{ -5, 6, 9, 1, 2, -3 };
+            int num = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    num++;
+                }
+            }
+
+            Console.WriteLine($"Количество положительных чисел: {num}");
+
+        }
+    }
+}
