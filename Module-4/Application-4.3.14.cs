586ebd7aa96dff02cdefc9fac7aefa09798d8eba
Tamer <tamer.masud@mail.ru>
2024-02-15 22:38:01 +0400

    Добавлено задание 4.3.14

---
+++ Module-4/Application-4.3.14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module_4
+{
+    internal class Application_4_3_14
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[3][];
+
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach (var a in array)
+            {
+                foreach (var item in a)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
